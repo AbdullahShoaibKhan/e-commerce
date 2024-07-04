7e3f7896e677b40e490be828469f49fa11d2dab4
AbdullahShoaibKhan <abdullahshoaib0333@gmail.com>
2024-07-04 18:16:16 +0500

    Deletion

--- src/providers/next-progress.tsx
+++
@@ -1,8 +0,0 @@
-// "use client";
-
-// import { AppProgressBar as ProgressBar } from "next-progress-bar";
-// const NextProgress = () => {
-//   return <ProgressBar color="#01C4FF" />;
-// };
-
-// export default NextProgress;
--- src/providers/next-theme.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { ThemeProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
-
-const NextTheme = ({ children, ...props }: ThemeProviderProps) => {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>;
-};
-
-export default NextTheme;
